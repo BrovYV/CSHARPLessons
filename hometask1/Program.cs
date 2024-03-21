fc4efbf53211be3be80950543cf1fb96d75f7b7f
Yuri Brovko <yuribrovko@iMac-Yuri.local>
2024-03-21 22:39:46 +0300

    first commit

---
+++ hometask1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine((num1) + " " + "большее число");
+    Console.WriteLine((num2) + " " + "меньшее число");
+}
+if (num1 < num2)
+{
+    Console.WriteLine((num2) + " " + "большее число");
+    Console.WriteLine((num1) + " " + "меньшее число");
+}
+else
+{
+    if (num1 == num2)
+    {
+      Console.WriteLine("числа равны");  
+    }
+    
+}
