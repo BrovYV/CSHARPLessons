8fbe98d36f520d7a87ef59e90da452f3a2b74306
Yuri Brovko <yuribrovko@iMac-Yuri.local>
2024-03-25 23:45:21 +0300

    added tasks of sem4

---
+++ sem4_task3/Program.cs
@@ -0,0 +1,69 @@
+﻿// Заполните массив на N (вводится ли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из массива. Старший разряд числа находится
+// на 0-м индексе, младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+
+int ArrToNum(int[] array)
+{
+    int num = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        num = num * 10 + array[i];
+    }
+    return num;
+}
+
+Console.Write("Введите размер массива но не более 8:");
+
+int size = Convert.ToInt32(Console.ReadLine());
+if (size < 1 || size >8)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+
+
+int[] arr = CreateArrayRndInt(size, 1, 10);
+PrintArray(arr);
+
+int result = ArrToNum(arr);
+Console.WriteLine();
+Console.WriteLine(result);
